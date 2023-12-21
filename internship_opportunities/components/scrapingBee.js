78d9fc08fc71576b026a568e4aa5ecfd671cc8a8
rishi <71474438+rishicantcode@users.noreply.github.com>
2023-12-21 12:29:23 -0800

    new

--- internship_opportunities/components/scrapingBee.js
+++ internship_opportunities/components/scrapingBee.js
@@ -3,9 +3,10 @@ const axios = require('axios');
 axios.get('https://app.scrapingbee.com/api/v1/store/google', {
     params: {
         'api_key': '2ZM4IM8LIKH7O15OI3Y5SB43Z863WNVRR5L0IJAJDL42B7H97UZFWB8UU2MWSQAPMBUEERV1OH07CZ35',
-        'search': 'internships',
+        'search': 'high school internships',
     }
 }).then(function (response) {
     // handle success
-    console.log(response.data);
+    const data = response.data;
+    console.log(data);
 })
