7b0384b123cdedc2be7a66e82cf870cf652c9f18
rishi <71474438+rishicantcode@users.noreply.github.com>
2024-01-07 11:40:13 -0800

    intergation with user no  hard code (pls check if works)

--- internship_opportunities_flask/server.py
+++ internship_opportunities_flask/server.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import the CORS extension
+import requests
+from scrapingbee import ScrapingBeeClient
+
+
+client = ScrapingBeeClient(api_key='NH07W9TZ6UWUI3YGDKE27YH736KMTFTLRN2PDK8G057JWGJ03ABA8DT7J0XVEY33JYTZ7P91XI8VVKPY')
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -28,12 +33,51 @@ def save_data():
 
         formatted_location = f"{city}, {state}, {country}"
         finalSearchQuery = selectedGrade + " internship opportunities for "+listInternshipTypes+"in "+formatted_location
+        
+        response = requests.get(
+            url="https://app.scrapingbee.com/api/v1/store/google",
+            params={
+                "api_key": "NH07W9TZ6UWUI3YGDKE27YH736KMTFTLRN2PDK8G057JWGJ03ABA8DT7J0XVEY33JYTZ7P91XI8VVKPY",
+                "search": finalSearchQuery,
+                "nb_results": "5"  # Set the number of results to 5
+            },
+        )
+        print('Response HTTP Status Code:', response.status_code)
+
+        if response.status_code == 200:
+            # Parse the JSON content
+            data = response.json()
+
+            i = 0
+
+            # Extract and print the URLs
+            for result in data.get("organic_results", []):
+                urls = result.get("url")
+                screen = client.get(
+                    url=urls,
+                    params={
+                        "screenshot_full_page": "true",
+                    },
+                )
+
+                if screen.status_code == 200:
+                    with open('screenshot{}.png'.format(i), 'wb') as f:
+                        i += 1
+                        f.write(screen.content)
+                    print("Image saved")
+
+                else:
+                    print("Failed to save", screen.status_code)
+            
         print(finalSearchQuery)
         return jsonify({'message': 'Data received successfully.'}), 200
     except Exception as e:
         print('Error processing data:', str(e))
         return jsonify({'error': 'Failed to process data.'}), 500
     
+
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
