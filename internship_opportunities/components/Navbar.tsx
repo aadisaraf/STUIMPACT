a0616eb7f9d1e10937001c001d712e8f4aa55bc1
aadisaraf <104963025+aadisaraf@users.noreply.github.com>
2024-03-31 13:32:12 -0700

    Add files via upload

---
+++ internship_opportunities/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="bg-blue-800 p-4">
+            <ul className="flex justify-evenly text-2xl font-bold">
+                <li><Link href="/">| Home |</Link></li>
+                <li><Link href="/api/auth/signin">| Sign In |</Link></li>
+                <li><Link href="/api/auth/signout">| Sign Out| </Link></li>
+            </ul>
+        </nav>
+    );
+}
---
+++ internship_opportunities/components/UserCard.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+
+type User = {
+    name?: string | null | undefined;
+    email?: string |null|undefined;
+    image?: string |null|undefined;
+} | undefined
+
+type Props = {
+    user:User,
+    pagetype:string,
+}
+
+export default function Card({ user, pagetype }: Props) {
+    const greeting = user?.name ?(
+        <div className = "flex flex-col items-center p-6 rounded-lg font-bold text-5xl text-black">
+            Hello {user?.name}!
+        </div>
+
+    ) : null
+    // const userImage = user?.image ?(
+    //     <Image
+    //     className='border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8'
+    //     src = {user?.image}
+    //     width = {200}
+    //     height = {200}
+    //     alt = {user?.name ?? "Profile Pic"}
+    //     priority = {true}
+    //     />
+    // ) : null
+    return(
+        <section className='flex text-2xl flex-col gap-4'>
+            {greeting}
+            {/* {userImage} */}
+            
+        </section>
+    )
+}
